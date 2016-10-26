781bccf83f4932fddd147ffbc055247c435e0656
davorivas <davorivas@gmail.com>
2016-10-26 13:24:38 -0500

    KNOW-292 initial work

---
+++ widget-composer/src/Widget.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="widget">
+    <h1>WIDGET</h1>
+    <img src="./assets/logo.png">
+    <scaffold></scaffold>
+  </div>
+</template>
+
+<script>
+import Scaffold from './components/Scaffold'
+
+export default {
+  name: 'widget',
+  components: {
+    Scaffold
+  }
+}
+</script>
+
+<style>
+#widget {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- widget-composer/src/main.js
+++ widget-composer/src/main.js
@@ -1,9 +1,16 @@
 import Vue from 'vue'
 import App from './App'
+import Widget from './Widget'
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   template: '<App/>',
   components: { App }
+})
+
+new Vue({
+  el: '#widget',
+  template: '<Widget/>',
+  components: { Widget }
 })
