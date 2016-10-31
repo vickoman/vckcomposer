fd962d1661a01a2d1b97aad36266110d3815ecb3
David Torroija <davidtorroija@gmail.com>
2016-10-31 10:50:30 -0500

    KNOW-255 Quick Actions (Toolbar): Undo & Redo: fixed jquery

--- widget-composer/build/webpack.base.conf.js
+++ widget-composer/build/webpack.base.conf.js
@@ -2,7 +2,7 @@ var path = require('path')
 var config = require('../config')
 var utils = require('./utils')
 var projectRoot = path.resolve(__dirname, '../')
-
+var webpack = require('webpack')
 var env = process.env.NODE_ENV
 // check env & config/index.js to decide weither to enable CSS Sourcemaps for the
 // various preprocessor loaders added to vue-loader at the end of this file
@@ -11,6 +11,12 @@ var cssSourceMapProd = (env === 'production' && config.build.productionSourceMap
 var useCssSourceMap = cssSourceMapDev || cssSourceMapProd
 
 module.exports = {
+  plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        })
+  ],
   entry: {
     app: './src/main.js'
   },
